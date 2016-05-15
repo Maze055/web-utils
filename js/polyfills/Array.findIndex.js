52866405c732e05affa366b1440bf7bfd30e53a8
Davide Laezza <truzzialrogo@gmx.com>
2016-05-15 17:29:48 +0200

    Added third-part made JavaScript polyfills.

---
+++ js/polyfills/Array.findIndex.js
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview
+ *
+ * Array.prototype.findIndex() polyfill, by
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex MDN}.
+ */
+
+Array.prototype.findIndex = function(predicate) {
+	if (this === null) {
+		throw new TypeError('Array.prototype.findIndex called on null or undefined');
+	}
+	if (typeof predicate !== 'function') {
+		throw new TypeError('predicate must be a function');
+	}
+	var list = Object(this);
+	var length = list.length >>> 0;
+	var thisArg = arguments[1];
+	var value;
+
+	for (var i = 0; i < length; i++) {
+		value = list[i];
+		if (predicate.call(thisArg, value, i, list)) {
+			return i;
+		}
+	}
+	return -1;
+};
